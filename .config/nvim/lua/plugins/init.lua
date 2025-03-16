3ac96acbb04d24376256379a2a6b4ca82071188a
Jörn Hirschfeld <joern@hirschfeld.tech>
2025-03-16 21:20:37 +0100

    I guess rust debugging works

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -25,9 +25,44 @@ return {
     "mrcjkb/rustaceanvim",
     version = '^5', -- Recommended
     lazy = false, -- This plugin is already lazy
+    config = function()
+      local cfg = require("rustaceanvim.config")
+      vim.g.rustaceanvim = {
+        dap = {
+          adapter = cfg.get_codelldb_adapter("codelldb", "/usr/lib/codelldb/lldb/lib/liblldb.so")
+        },
+      }
+    end,
+    ["rust-analyzer"] = {
+      cargo = {
+        allFeatures = true,
+      }
+    },
   },
   {
     "mfussenegger/nvim-dap",
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.open()
+      end
+    end
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+    config = function()
+      require("dapui").setup()
+    end,
   }
 
   -- {
